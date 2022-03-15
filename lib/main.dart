c5e34dc1b7c7ace69fbef029a7aeea3885ea9b7e
Edilson Matola <edilsonmatola13@gmail.com>
2022-03-15 14:06:57 +0200

    Changed Color Theme of the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color.fromRGBO(7, 94, 84, 1),
         colorScheme: ColorScheme.fromSwatch(
-          accentColor: Color.fromRGBO(18, 140, 126, 1),
+          accentColor: Color.fromRGBO(37, 211, 102, 1),
           cardColor: Color.fromRGBO(37, 211, 102, 1),
         ),
         floatingActionButtonTheme: FloatingActionButtonThemeData(
